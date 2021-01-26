f8a9873b438fed6e67b540411785f1dd2da23a60
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-01-26 12:32:38 +0100

    Unit Test

--- OperazioniTest/GestioneTest.cs
+++ OperazioniTest/GestioneTest.cs
@@ -8,36 +8,28 @@ namespace OperazioniTest
         [TestMethod]
         public void TestSommaPositivi()
         {
-            int a = 3;
-            int b = 7;
-            int somma_aspettata = 10;
+            int a = 3, b = 7, somma_aspettata = 10;
             int somma_effettiva = Gestione.Somma(a, b);
             Assert.AreEqual(somma_aspettata, somma_effettiva);
         }
         [TestMethod]
         public void TestSommaNulli()
         {
-            int a = 0;
-            int b = 0;
-            int somma_aspettata = 0;
+            int a = 0, b = 0, somma_aspettata = 0;
             int somma_effettiva = Gestione.Somma(a, b);
             Assert.AreEqual(somma_aspettata, somma_effettiva);
         }
         [TestMethod]
         public void TestSommaNegativi()
         {
-            int a = -2;
-            int b = -5;
-            int somma_aspettata = -7;
+            int a = -2, b = -5, somma_aspettata = -7;
             int somma_effettiva = Gestione.Somma(a, b);
             Assert.AreEqual(somma_aspettata, somma_effettiva);
         }
         [TestMethod]
         public void TestSommaPosENeg()
         {
-            int a = -10;
-            int b = 1;
-            int somma_aspettata = -9;
+            int a = -10, b = 1, somma_aspettata = -9;
             int somma_effettiva = Gestione.Somma(a, b);
             Assert.AreEqual(somma_aspettata, somma_effettiva);
         }
